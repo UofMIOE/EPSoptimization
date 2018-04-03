efd960785c0ed4f52e42abe77b914219e0e2edd5
Matt Schiele <mschiele@umich.edu>
2018-04-03 12:17:40 -0400

    Cuckoo Search Changes
    Algorithm works now.  Converted from minimization to maximization

--- Cuckoo_Search_Algorithm.py
+++ Cuckoo_Search_Algorithm.py
@@ -11,7 +11,7 @@ class CuckooSearch(BaseAlgorithm):
     def __init__(self, function_wrapper, number_of_variables = 1, objective = "maximization"):
         super().__init__(function_wrapper,number_of_variables,objective)
 
-    def search(self, n=100, iteration=50, pa=0.25,
+    def search(self, n=100, iteration=100, pa=0.25,
                  nest=100):
         """
         :param n: number of agents
@@ -25,8 +25,9 @@ class CuckooSearch(BaseAlgorithm):
         :param ub: upper limits for plot axes        
         """
 
-        dimension = self.number_of_variables
-#        super(CuckooSearch, self).__init__(function_wrapper, number_of_variables, objective)
+#        dimension = self.number_of_variables
+        dimension = 2
+        super(CuckooSearch, self).__init__(self.function_wrapper, self.number_of_variables, self.objective)
 
         self.__Positions = []
         self.__Gbest = []
@@ -57,9 +58,9 @@ class CuckooSearch(BaseAlgorithm):
 #            self.__nests.append(oneNest)
 #        self.__nests = np.array(self.__nests)
 
-        self.__agents = np.random.uniform(function_wrapper.minimum_decision_variable_values()[0], function_wrapper.maximum_decision_variable_values()[0], (n, dimension))
-        self.__nests = np.random.uniform(function_wrapper.minimum_decision_variable_values()[0], function_wrapper.maximum_decision_variable_values()[0], (nest, dimension))
-        Pbest = self.__nests[np.array([function_wrapper.objective_function_value(x)
+        self.__agents = np.random.uniform(self.function_wrapper.minimum_decision_variable_values()[0], self.function_wrapper.maximum_decision_variable_values()[0], (n, dimension))
+        self.__nests = np.random.uniform(self.function_wrapper.minimum_decision_variable_values()[0], self.function_wrapper.maximum_decision_variable_values()[0], (nest, dimension))
+        Pbest = self.__nests[np.array([self.function_wrapper.objective_function_value(x)
                                        for x in self.__nests]).argmin()]
         Gbest = Pbest
         self._points(self.__agents)
@@ -68,12 +69,12 @@ class CuckooSearch(BaseAlgorithm):
 
             for i in self.__agents:
                 val = randint(0, nest - 1)
-                if function_wrapper.objective_function_value(i) < function_wrapper.objective_function_value(self.__nests[val]):
+                if self.function_wrapper.objective_function_value(i) < self.function_wrapper.objective_function_value(self.__nests[val]):
                     self.__nests[val] = i
 
-            fnests = [(function_wrapper.objective_function_value(self.__nests[i]), i) for i in range(nest)]
+            fnests = [(self.function_wrapper.objective_function_value(self.__nests[i]), i) for i in range(nest)]
             fnests.sort()
-            fcuckoos = [(function_wrapper.objective_function_value(self.__agents[i]), i) for i in range(n)]
+            fcuckoos = [(self.function_wrapper.objective_function_value(self.__agents[i]), i) for i in range(n)]
             fcuckoos.sort(reverse=True)
 
             nworst = nest // 2
@@ -81,21 +82,21 @@ class CuckooSearch(BaseAlgorithm):
 
             for i in worst_nests:
                 if random() < pa:
-                    self.__nests[i] = np.random.uniform(function_wrapper.minimum_decision_variable_values()[0], function_wrapper.maximum_decision_variable_values()[0], (1, dimension))
+                    self.__nests[i] = np.random.uniform(self.function_wrapper.minimum_decision_variable_values()[0], self.function_wrapper.maximum_decision_variable_values()[0], (1, dimension))
 #                    oneNest = list()
 #                    for j in range(dimension):
 #                        oneNest.append(np.random.uniform(function_wrapper.minimum_decision_variable_values()[j], function_wrapper.maximum_decision_variable_values()[j])
 #                    oneNest = np.array(oneNest)
 #                    self.__nests[i] = oneNest
 
-            if nest > n:
+            if nest < n:
                 mworst = n
             else:
                 mworst = nest
 
             for i in range(mworst):
 
-                if fnests[i][0] < fcuckoos[i][0]:
+                if fnests[i][0] > fcuckoos[i][0]:
                     self.__agents[fcuckoos[i][1]] = self.__nests[fnests[i][1]]
 
 
@@ -108,7 +109,7 @@ class CuckooSearch(BaseAlgorithm):
 #                    else 
 #                        self.__nests[i][j] = function_wrapper.maximum_decision_variable_values()[i]
                     
-            self.__nests = np.clip(self.__nests, function_wrapper.minimum_decision_variable_values()[0], function_wrapper.maximum_decision_variable_values()[0])
+            self.__nests = np.clip(self.__nests, self.function_wrapper.minimum_decision_variable_values()[0], self.function_wrapper.maximum_decision_variable_values()[0])
             self.__Levyfly(step, Pbest, n, dimension)
 
 #            for j in range(n):
@@ -119,19 +120,19 @@ class CuckooSearch(BaseAlgorithm):
 #                        self.__agents[i][j] = function_wrapper.minimum_decision_variable_values()[i]
 #                    else 
 #                        self.__agents[i][j] = function_wrapper.maximum_decision_variable_values()[i]
-            self.__agents = np.clip(self.__agents, function_wrapper.minimum_decision_variable_values()[0], function_wrapper.maximum_decision_variable_values()[0])
+            self.__agents = np.clip(self.__agents, self.function_wrapper.minimum_decision_variable_values()[0], self.function_wrapper.maximum_decision_variable_values()[0])
             self._points(self.__agents)
             self.__nest()
 
-            Pbest = self.__nests[np.array([function_wrapper.objective_function_value(x)
-                                        for x in self.__nests]).argmin()]
+            Pbest = self.__nests[np.array([self.function_wrapper.objective_function_value(x)
+                                        for x in self.__nests]).argmax()]
 
-            if function_wrapper.objective_function_value(Pbest) < function_wrapper.objective_function_value(Gbest):
+            if self.function_wrapper.objective_function_value(Pbest) > self.function_wrapper.objective_function_value(Gbest):
                 Gbest = Pbest
 
         self._set_Gbest(Gbest)
 
-        return {"best_decision_variable_values": self.get_Gbest(), "best_objective_function_value":self.function_wrapper.objective(self.get_Gbest())}
+        return {"best_decision_variable_values": self.get_Gbest(), "best_objective_function_value":self.function_wrapper.objective_function_value(self.get_Gbest())}
 
     def __nest(self):
         self.__Nests.append([list(i) for i in self.__nests])
